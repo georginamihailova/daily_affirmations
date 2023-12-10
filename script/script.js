26f3dd50f688ae06398f185d507524523fb99879
Georgina Mihailova <georginamihailova@hotmail.com>
2023-12-10 19:36:51 +0100

    changes in script made

--- script/script.js
+++ script/script.js
@@ -1,3 +1,4 @@
+// Function to open the popup window
 function displayPopup() {
     var popup = document.getElementById("popup");
     popup.style.display = "block";
@@ -16,31 +17,31 @@ function closePopup() {
     body.classList.remove("popup-open");
 }
 
-// Call the displayPopup function when the website is opened
 window.onload = displayPopup;
-    function refreshCards() {
+
+function refreshCards() {
     var cards = document.querySelectorAll(".card");
 
     cards.forEach(function (card) {
-    var mainText = card.querySelector(".main-text");
-    var hiddenText = card.querySelector(".hidden-text");
+        var mainText = card.querySelector(".main-text");
+        var hiddenText = card.querySelector(".hidden-text");
 
-    var mainTextDisplay = getComputedStyle(mainText).display;
+        var mainTextDisplay = getComputedStyle(mainText).display;
 
-    if (mainTextDisplay === "none" || mainTextDisplay === "") {
-    mainText.style.display = "block";
-    hiddenText.style.display = "none";
-} else {
-    mainText.style.display = "none";
-    hiddenText.style.display = "block";
-}
+        if (mainTextDisplay === "none" || mainTextDisplay === "") {
+            mainText.style.display = "block";
+            hiddenText.style.display = "none";
+        } else {
+            mainText.style.display = "none";
+            hiddenText.style.display = "block";
+        }
 
-    card.classList.add("shake");
-    setTimeout(() => card.classList.remove("shake"), 500); // Remove the class after the animation duration (in milliseconds)
-});
+        card.classList.add("shake");
+        setTimeout(() => card.classList.remove("shake"), 500);
+    });
 
     var refreshBtn = document.querySelector(".refresh-btn");
-    refreshBtn.classList.add("rotate"); // Add the rotate class to trigger the
-    setTimeout(() => refreshBtn.classList.remove("rotate"), 500); // Remove the class after the animation duration (in milliseconds)
+    refreshBtn.classList.add("rotate");
+    setTimeout(() => refreshBtn.classList.remove("rotate"), 500);
 }
 
